13a21ddc9628f46e5e3661f5692869f2a281daed
Allan <sechristallan@gmail.com>
2022-02-27 10:50:07 +0900

    add env var for postgres database

--- application.py
+++ application.py
@@ -3,18 +3,19 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
-from secret_keys import SECRET_KEY
+# from secret_keys import SECRET_KEY
+import os
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor= CKEditor(app)
 Bootstrap(app)
 
 #connect to database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
