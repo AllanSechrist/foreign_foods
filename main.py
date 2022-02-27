b6420ed2a93e7b622486054e0aff230acd7bbf16
Allan <sechristallan@gmail.com>
2022-02-27 15:40:25 +0900

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from click import style
 from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_user, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
