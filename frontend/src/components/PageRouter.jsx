fce44de4e3a9c1ea61b66cd6197c6de31a32f5e4
AllanSechrist <sechristallan@gmail.com>
2022-05-15 23:16:43 +0900

    add new blog route, add new blog form

--- frontend/src/components/PageRouter.jsx
+++ frontend/src/components/PageRouter.jsx
@@ -4,6 +4,7 @@ import Blogs from "./pages/Blogs"
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
 import NewRestaurant from './pages/NewRestaurant'
+import NewBlog from './pages/NewBlog'
 import Header from "./Header";
 import Footer from "./Footer";
 import React from 'react'
@@ -18,12 +19,14 @@ function PageRouter() {
                 <Link to="/">Home</Link>
                 <Link to="/restaurants">Restaurants</Link>
                 <Link to="/new-restaurant">Add Restaurant</Link>
+                <Link to="/new-blog">Add Blog</Link>
                 <Link to="/blog">Blogs</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/restaurants" element={<Restaurants/>} />
                 <Route path="/new-restaurant" element={<NewRestaurant/>}/>
+                <Route path="/new-blog" element={<NewBlog/>} />
                 <Route path="/blog" element={<Blogs />} />
                 <Route path="/blog/:blogId" element={<Blog />} />
             </Routes>
--- frontend/src/components/pages/NewBlog.jsx
+++ frontend/src/components/pages/NewBlog.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import {Form, Field} from 'react-final-form'
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+    await sleep(300)
+    window.alert(JSON.stringify(values, 0, 2))
+}
+
 function NewBlog() {
     return (
-        <h1>Add new blog</h1>
+        <div>
+            <h1>Add new blog</h1>
+            <Form 
+                onSubmit={onSubmit}
+                validate = {values => {
+                    const errors = {}
+                    if(!values.title) {
+                        errors.title = "Required"
+                    }
+                    if(!values.subtitle) {
+                        errors.subtitle = "Required"
+                    }
+                    if(!values.body) {
+                        errors.body = "Required"
+                    }
+                    return errors
+                    
+                }}
+                render={({ handleSubmit, form, submitting, pristine, values}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="title">
+                            {({ input, meta}) => (
+                                <div>
+                                    <label>Title</label>
+                                    <input {...input} type="text" placeholder="Title" />
+                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                                </div>
+                            )}
+                        </Field>
+                        <Field name="subtitle">
+                            {({ input, meta}) =>(
+                                <div>
+                                    <label>Subtitle</label>
+                                    <input {...input} type="text" placeholder="Subtitle" />
+                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                                </div>
+                            )}
+                        </Field>
+                        <Field name="body" component="textarea">
+                            {({ input, meta}) => (
+                                <div>
+                                    <label>Body</label>
+                                    <input {...input} placeholder="Body" />
+                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                                </div>
+                            )}
+                        </Field>
+                        <button type="submit">
+                            Submit
+                        </button>
+                        <button type="button" onClick={form.reset} disabled={submitting || pristine}>
+                            Reset
+                        </button>
+                        <pre>{JSON.stringify(values, 0, 2)}</pre>
+                    </form>
+                )}
+            />
+
+            
+        </div>
+        
     )
 }
 
