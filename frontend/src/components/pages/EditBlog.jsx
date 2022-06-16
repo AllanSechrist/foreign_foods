c1c2371a1e1a608c006ef30820897db4bfe3aa38
AllanSechrist <sechristallan@gmail.com>
2022-06-16 22:34:09 +0900

    clean up EditBlog.jsx

--- frontend/src/components/pages/EditBlog.jsx
+++ frontend/src/components/pages/EditBlog.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import BlogForm from '../BlogForm'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+
 
 
 function EditBlog() {
